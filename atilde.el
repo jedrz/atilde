493b84d5cbadbf0fc9d0318b8e9593a439a96c7c
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-01-23 01:40:43 +0100

    Ignore some environments

--- atilde.el
+++ atilde.el
@@ -3,6 +3,7 @@
 ;; Version: 0.0.0
 
 (require 'dash)
+(require 'expand-region)
 
 (defvar atilde-words '("a" "e" "i" "o" "u" "w" "z"
                        "od" "nad" "pod"))
@@ -16,12 +17,36 @@
                                         (concat "^" f "\\| " f))
                                       (cdr atilde-words))))
 
+(defvar atilde-ignored-envs '("\\begin{displaymath}"
+                              "\\begin{displaystyle}"))
+
+(defvar atilde-ignore-regexp (concat
+                              "\\("
+                              (s-join
+                               "\\|"
+                               (--map
+                                (s-replace "\\" "\\\\")
+                                atilde-ignored-envs))
+                              "\\)"))
+
+(defun atilde-in-ignored-env? ()
+  (save-excursion
+    (let ((expand-region-fast-keys-enabled nil)
+          (stop nil))
+      (while (and (not stop)
+                  (> (point) (point-min)))
+        (er/expand-region 1)
+        (when (looking-at atilde-ignore-regexp)
+          (setq stop t)))
+      stop)))
+
 (defun atilde-insert-tilde? ()
   (looking-back atilde-regexp (line-beginning-position)))
 
 (defun atilde-space ()
   (interactive)
-  (insert (if (atilde-insert-tilde?) "~" " ")))
+  (insert (if (and (atilde-insert-tilde?)
+                   (not (atilde-in-ignored-env?))) "~" " ")))
 
 (provide 'atilde)
 
