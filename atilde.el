c145379f9c3310eac0c52e1f8ff3e7f3df927c5d
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-01-24 23:07:26 +0100

    Add docstrings

--- atilde.el
+++ atilde.el
@@ -5,24 +5,35 @@
 (require 'dash)
 (require 'expand-region)
 
-(defvar atilde-words '("a" "e" "i" "o" "u" "w" "z"
-                       "od" "nad" "pod"))
+(defvar atilde-words
+  '("a" "e" "i" "o" "u" "w" "z"
+    "od" "nad" "pod")
+  "A list of words after which tilde can be inserted.")
 
-(defvar atilde-regexp (format
-                       "\\<\\(%s\\)"
-                       (s-join "\\|" atilde-words)))
+(defvar atilde-regexp
+  (format
+   "\\<\\(%s\\)"
+   (s-join "\\|" atilde-words))
+  "A regexp detecting `atilde-words'.")
 
-(defvar atilde-ignored-envs '("\\begin{displaymath}"
-                              "\\begin{displaystyle}"))
+(defvar atilde-ignored-envs
+  '("\\begin{displaymath}"
+    "\\begin{displaystyle}")
+  "A list of ignored environments.")
 
-(defvar atilde-ignore-regexp (format
-                              "\\(%s\\)"
-                              (s-join "\\|"
-                                      (--map
-                                       (s-replace "\\" "\\\\" it)
-                                       atilde-ignored-envs))))
+(defvar atilde-ignore-regexp
+  (format
+   "\\(%s\\)"
+   (s-join "\\|"
+           (--map
+            (s-replace "\\" "\\\\" it)
+            atilde-ignored-envs)))
+  "A regexp to check if we are looking at the beginning of an environment.")
 
 (defun atilde-in-ignored-env? ()
+  "Check if point is in an ignored environment.
+
+See `atilde-ignored-envs' for a list of ignored environments."
   (save-excursion
     (let ((expand-region-fast-keys-enabled nil)
           (stop nil))
@@ -34,13 +45,20 @@
       stop)))
 
 (defun atilde-insert-tilde-after-word? ()
+  "Check if tilde can be inserted at point."
   (looking-back atilde-regexp (line-beginning-position)))
 
 (defun atilde-insert-tilde? ()
+  "Check if tilde can be inserted at point."
   (and (atilde-insert-tilde-after-word?)
        (not (atilde-in-ignored-env?))))
 
 (defun atilde-space ()
+  "Insert tilde or space.
+
+Tilde is inserted after `atilde-words'. If the point is in an
+ignored environment (see `atilde-ignored-envs') then always space
+is inserted."
   (interactive)
   (when (atilde-insert-tilde?)
     (setq last-command-event ?~))
