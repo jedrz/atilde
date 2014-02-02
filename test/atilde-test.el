083d9eacf26c8751433593e46ca1fe121845a29e
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-02-02 23:21:15 +0100

    Document ert tests

--- test/atilde-test.el
+++ test/atilde-test.el
@@ -13,6 +13,7 @@
      ,@body))
 
 (ert-deftest atilde-test/get-missing-tildes-positions ()
+  "Test reported spaces."
   (atilde-test-with-text "foo a z bar od word"
     (let ((result-positions (atilde-get-missing-tildes-positions))
           (real-positions '(6 8 15)))
@@ -23,6 +24,7 @@
           (should (= result real)))))))
 
 (ert-deftest atilde-test/overlays-positions ()
+  "Test if overlays are properly placed."
   (atilde-test-with-text "foo a z bar od word"
     (let ((overlays (sort (atilde-overlays-in (point-min) (point-max))
                           (lambda (o1 o2)
@@ -38,11 +40,13 @@
           (should (= end (1+ pos))))))))
 
 (ert-deftest atilde-test/delete-overlays-after-turning-off ()
+  "Test if overlays are removed after turning off atilde mode."
   (atilde-test-with-text "a foo"
     (atilde-mode -1)
     (should-not (atilde-overlays-in (point-min) (point-max)))))
 
 (ert-deftest atilde-test/add-overlays-after-turning-on ()
+  "Test if overlays are removed after turning on atilde mode."
   (atilde-test-with-text "a foo"
     (let* ((overlay (car (atilde-overlays-in (point-min) (point-max))))
            (beg (overlay-start overlay)))
